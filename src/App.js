c1400d805ffd3f8c93ef645859b749d29a719879
Muksud Hussain Mahi <mmahi1@mycentennialcollege.ca>
2022-02-03 13:46:52 -0500

    Login page added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes, Redirect } from "react-router-dom";
+import Login from "./components/Login/Login";
+import Feedback from "./components/Feedback/Feedback";
+import Thanks from "./components/Thanks/Thanks";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />}></Route>
+          <Route path="/feedback" element={<Feedback />}></Route>
+          <Route path="/thanks" element={<Thanks />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Login/Login.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Login.css";
+
+export default function Login(props) {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("I am here");
+    navigate("/feedback");
+  };
+  return (
+    <FormLayout
+      handleSubmit={handleSubmit}
+      setEmail={setEmail}
+      setPassword={setPassword}
+    />
+  );
+}
+
+function FormLayout(props) {
+  return (
+    <div className="Login">
+      <Form onSubmit={props.handleSubmit}>
+        <Form.Group className="mb-3" size="lg" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={(event) => props.setEmail(event.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" size="lg" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={(event) => props.setPassword(event.target.value)}
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Login
+        </Button>
+      </Form>
+    </div>
+  );
+}
